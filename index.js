5c65530d205e4c3fe65cb6607f8b0fded0856fc2
Mitesh <miteshdevganiya177@gmail.com>
2025-04-02 06:40:01 +0100

    solve error

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage,
-    limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
+    limits: { fileSize: 20 * 1024 * 1024 },
 });
 
 // 📥 DOCX → PDF
